f0fc610089b23d02675da6a436f91419d9f44ecb
Yiyi Wang <shd101wyy@sina.com>
2013-11-29 03:10:59 -0500

    huhhhh

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 /*
 	used to generate sudoku
-	9*9
+	n * n
 	with all values = 0
 */
-var generate_empty_sudoku = function()
+var generate_empty_sudoku = function(n)
 {
 	var output = [];
-	for(var i = 0; i < 9; i++)
+	for(var i = 0; i < n; i++)
 	{
 		output.push([]);
-		for(var j = 0; j < 9; j++)
+		for(var j = 0; j < n; j++)
 		{
 			output[i].push(0);
 		}
@@ -17,44 +17,49 @@ var generate_empty_sudoku = function()
 	return output;
 }
 /*
+	check whether has repetition at current i, j 
+	by row and col
+*/
+var have_repetition = function(sudoku_table, i, j, num)
+{
+	/* check row */
+	for(var a = 0; a < sudoku_table.length; a++)
+	{
+		if(a == i) continue;
+		if(sudoku_table[a][j] === num)
+			return true
+	}
+	/* check col */
+	for(var a = 0; a < sudoku_table.length; a++)
+	{
+		if(a == j) continue;
+		if(sudoku_table[i][a] === num)
+			return true;
+	}
+	/* check square */
+	var n = Math.sqrt(sudoku_table.length);
+	var x = Math.floor(i/n) * n;
+	var y = Math.floor(j/n) * n;
+	for(var a = x; a < x+n; a++)
+	{
+		for(var b = y; b < y+n; b++)
+		{
+			if(a == i && b == j) continue;
+			if(sudoku_table[a][b] === num) return true;
+		}
+	}
+	return false;
+}
+
+/*
 	3*3
 	在3*3中随机选取四个数并且付值1～9
 
 	used to generate random sudoku table
 */
-var generate_random_sudoku = function()
+var generate_random_sudoku = function(n)
 {
-	var empty_sudoku_table = generate_empty_sudoku();
-	var have_repetition = function(sudoku_table, i, j, num)
-	{
-		/* check row */
-		for(var a = 0; a < i; a++)
-		{
-			if(sudoku_table[a][j] === num)
-				return true
-		}
-		/* check col */
-		for(var a = 0; a < j; a++)
-		{
-			if(sudoku_table[i][a] === num)
-				return true;
-		}
-		/*
-			check inside 3*3
-		*/
-		/*
-		var start_i = parseInt(i/3)*3;
-		var start_j = parseInt(j/3)*3;
-		for(var a = start_i; a < start_i+3; a++)
-		{
-			for(var b = start_j; b < start_j+3; b++)
-			{
-				if((a!==i && b!==j) && sudoku_table[a][b] === sudoku_table[i][j])
-					return true;
-			}
-		}*/
-		return false;
-	}
+	var empty_sudoku_table = generate_empty_sudoku(9);
 	/*
 
 		start from left-top corner
@@ -117,6 +122,7 @@ var generate_random_sudoku = function()
 		[3,1,2]
 		[3,2,1]
 */
+/*
 var generate_all_possible_combination = function(l)
 {
 	// length = 0;
@@ -146,208 +152,215 @@ var generate_all_possible_combination = function(l)
 		return output;
 	}
 }
-/*
-	CHECK WHETHER SUDOKU TABLE IS CORRECT
-	Only check rows and cols
 */
-var correct_sudoku = function(sudoku_table)
+
+
+/*
+	get value list that an element can take
+*/
+var get_available_value_list = function(sudoku_table, i, j)
 {
-	// check row
-	for(var i = 0; i < 9; i++)
+	var exist_ = {}; // check exist
+	for(var a = 0; a < sudoku_table.length; a++)
 	{
-		var table = {}; // length 9 table
-		for(var a = 0; a < 9; a++)
+		exist_[a+1] = false;
+	}
+
+	/* check row */
+	for(var a = 0; a < sudoku_table.length; a++)
+	{
+		if(sudoku_table[a][j] === 0) continue;
+		exist_[sudoku_table[a][j]] = true;
+	}
+	/* check col */
+	for(var a = 0; a < sudoku_table.length; a++)
+	{
+		if(sudoku_table[i][a] === 0) continue;
+		exist_[sudoku_table[i][a]] = true;
+	}
+	/* check square */
+	var n = Math.sqrt(sudoku_table.length);
+	var x = Math.floor(i/n) * n;
+	var y = Math.floor(j/n) * n;
+	for(var a = x; a < x+n; a++)
+	{
+		for(var b = y; b < y+n; b++)
 		{
-			table[a+1] = false;
-		}
-		for(var j = 0; j < 9; j++)
-		{
-			var num = sudoku_table[i][j];
-			if(table[num] == true)
-				return false
-			else
-				table[num] = true;
+			if(sudoku_table[a][b] === 0) continue;
+			exist_[sudoku_table[a][b]] = true;
 		}
 	}
-	// check col
-	// check row
-	for(var i = 0; i < 9; i++)
+	var output = [];
+	for(var i in exist_)
 	{
-		var table = {}; // length 9 table
-		for(var a = 0; a < 9; a++)
+		if(exist_[i] === false)
 		{
-			table[a+1] = false;
-		}
-		for(var j = 0; j < 9; j++)
-		{
-			var num = sudoku_table[j][i];
-			if(table[num] == true)
-				return false
-			else
-				table[num] = true;
+			output.push(parseInt(i));
 		}
 	}
-	return true;
+	return output;
 }
 
 /*
-	try one by one
+	solve sudoku by backtracking algothrithm
 */
 var solve_sudoku = function(sudoku_table)
 {
 	/*
-		return
-		0: numbers need to fill
-		1: need to fill x coord
-		2: need to fill y coord
+		save coord
+		[[x0, y0], [x1, y1], ...]
 	*/
-	var return_3_to_3_table_information = function(sudoku_table, left_top_x, left_top_y)
+	var coord = [];
+	var value_list = [];
+	var value_list_index = [];
+	// init coord
+	// init value list for each coord
+	for(var i = 0; i < sudoku_table.length; i++)
 	{
-		/*
-			get number that need to fill
-		*/
-		var numbers_need_to_fill = [];
-		var number_already_filled = [];
-		var need_to_fill_x_coord = [];
-		var need_to_fill_y_coord = [];
-		for(var i = left_top_x; i < left_top_x+3; i++)
+		for(var j = 0; j < sudoku_table.length; j++)
 		{
-			for(var j = left_top_y; j < left_top_y+3; j++)
+			if(sudoku_table[i][j] == 0)
 			{
-				if(sudoku_table[i][j]!==0)
-				{
-					number_already_filled.push(sudoku_table[i][j])
-				}
-				else
-				{
-					need_to_fill_x_coord.push(i);
-					need_to_fill_y_coord.push(j);
-				}
+				coord.push([i, j]) // save coord
+				var available_value_list = get_available_value_list(sudoku_table, i, j);
+				value_list.push(available_value_list); // save available value list
+				value_list_index.push(0);
 			}
 		}
-		for(var i = 1; i<=9; i++)
-		{
-			if(number_already_filled.indexOf(i) == -1)
-				numbers_need_to_fill.push(i);
-		}
-		return [numbers_need_to_fill, need_to_fill_x_coord, need_to_fill_y_coord]
 	}
+
 	/*
-		get 3*3 table information
+		num start from 1
+		until length
 	*/
-	var information_list = []; // its length should be 9
-	for(var a = 0; a <= 6; a=a+3)
+	var correct_sudoku = function(sudoku_table, coord)
 	{
-		for(var b = 0; b <= 6; b=b+3)
+		for(var i = 0; i < coord.length; i++)
 		{
-			var information = return_3_to_3_table_information(sudoku_table, a, b);
-			information_list.push(information);
+			var x = coord[i][0]; var y = coord[i][1];
+			if(have_repetition(sudoku_table, x, y, sudoku_table[x][y]))
+				return false
 		}
+		return true;
 	}
-	var numbers_need_to_fill_list = [];
-	var x_coord = [];
-	var y_coord = [];
-	for(var i = 0; i < information_list.length; i++)
+	var count = 0
+	while(1)
 	{
-		numbers_need_to_fill_list.push(information_list[i][0]);
-		x_coord.push(information_list[i][1]);
-		y_coord.push(information_list[i][2]);
-	}
-	/* 
-		check combinations
-	*/
-	var combinations = [];
-	var combinations_length_list = [];
-	for(var i = 0; i < numbers_need_to_fill_list.length; i++)
-	{
-		var c = generate_all_possible_combination(numbers_need_to_fill_list[i])
-		combinations.push(c);
-		combinations_length_list.push(c.length); // attention: here is length not length-1
-	}
+		if(count === coord.length) return sudoku_table; // finished
+		var x = coord[count][0]; var y = coord[count][1];
+		var available_value_list = value_list[count];
+		sudoku_table[x][y] = 0;
+		var satisfied = false;
 
-	// COUNT combibations
-	var count = 1;
-	for(var i = 0; i < combinations_length_list.length; i++)
-	{
-		count*=combinations_length_list[i];
-	}
-	console.log(count);
-	return "TRUE";
-	// console.log(combinations);
-	
-	for(var a0 = 0; a0<combinations[0].length; a0++)
-	{
-		for(var a1 = 0; a1<combinations[1].length; a1++)
+		if(value_list_index[count] === available_value_list.length)
 		{
-			for(var a2 = 0; a2<combinations[2].length; a2++)
+			value_list_index[count] = 0;
+			count-=1;
+			continue;
+		}
+		for(var i = value_list_index[count]; i < available_value_list.length; i++)
+		{
+			if(have_repetition(sudoku_table, x, y, available_value_list[i])) continue;
+			else
 			{
-				for(var a3 = 0; a3<combinations[3].length; a3++)
-				{
-					for(var a4=0; a4<combinations[4].length; a4++)
-					{
-						for(var a5=0; a5<combinations[5].length; a5++)
-						{
-							for(var a6=0; a6<combinations[6].length; a6++)
-							{
-								for(var a7=0; a7<combinations[7].length; a7++)
-								{
-									for(var a8=0; a8<combinations[8].length; a8++)
-									{
-										var l0 = combinations[0][a0];
-										var l1 = combinations[1][a1];
-										var l2 = combinations[2][a2];
-										var l3 = combinations[3][a3];
-										var l4 = combinations[4][a4];
-										var l5 = combinations[5][a5];
-										var l6 = combinations[6][a6];
-										var l7 = combinations[7][a7];
-										var l8 = combinations[8][a8];
-										var c = [l0, l1, l2, l3, l4, l5, l6, l7, l8];
-										for(var i = 0; i < 9; i++)
-										{
-											xs = x_coord[i];
-											ys = y_coord[i];
-											for(var j = 0; j < xs.length; j++)
-											{
-												x = xs[j];
-												y = ys[j];
-												sudoku_table[x][y] = c[i][j];
-											}
-										}
-										// console.log("Enter Here")
-										console.log(sudoku_table);
-										if(correct_sudoku(sudoku_table))
-										{
-											console.log("PASS");
-											return "PASS"
-										}
-
-									}
-								}
-							}
-						}
-					}
-				}
+				satisfied = true;
+				value_list_index[count] = i + 1;
+				sudoku_table[x][y] = available_value_list[i];
+				break;
 			}
+		}
+		/*
+		console.log(count);
+		console.log("X: "+x+" Y: "+y);
+		console.log(available_value_list);
+		console.log(sudoku_table);
+		*/
+		if(satisfied) count+=1; // run next;
+		else 
+		{
+			value_list_index[count] = 0;
+			count-=1;
 		}
 	}
 }
-/*
+
+
 var test = [ 
-	[5,3,4,6,7,8,9,1,2],
-	[6,7,2,1,9,5,3,4,8],
-	[1,9,8,3,4,2,5,6,7],
-	[8,5,9,7,6,1,4,2,3],
+	[5,3,0,6,7,8,9,1,2],
+	[6,0,2,1,9,5,3,4,8],
+	[1,9,8,0,4,2,5,6,7],
+	[8,5,9,7,0,1,0,2,3],
 	[4,2,6,8,5,3,7,9,1],
-	[7,1,3,9,2,4,8,5,6],
-	[9,6,1,5,3,7,2,8,4],
-	[2,8,7,4,1,9,6,3,5],
-	[3,4,5,2,8,6,1,7,9]
-];*/
+	[7,1,3,9,0,4,0,5,6],
+	[9,6,0,5,3,7,2,8,4],
+	[2,8,0,4,0,9,6,3,5],
+	[3,4,5,2,8,0,1,7,9]
+];
+var world_hardest_sudoku = 
+[
+	[8,0,0,0,0,0,0,0,0],
+	[0,0,3,6,0,0,0,0,0],
+	[0,7,0,0,9,0,2,0,0],
+	[0,5,0,0,0,7,0,0,0],
+	[0,0,0,0,4,5,7,0,0],
+	[0,0,0,1,0,0,0,3,0],
+	[0,0,1,0,0,0,0,6,8],
+	[0,0,8,5,0,0,0,1,0],
+	[0,9,0,0,0,0,4,0,0]
+
+]
+var world_hardest_sudoku2 = 
+[
+	[0,0,0,0,4,1,8,9,0],
+	[0,0,4,0,0,0,0,0,0],
+	[0,0,2,5,9,0,0,0,0],
+	[0,5,0,4,0,0,6,0,0],
+	[0,0,0,0,1,0,0,0,8],
+	[0,8,0,0,0,0,4,5,0],
+	[0,0,5,7,0,0,0,0,0],
+	[0,0,6,0,0,0,0,4,0],
+	[0,9,0,3,6,0,1,0,0]
+]
+var t = 
+[
+	[4,3,2,1,0,0,0,0,9],							
+	[0,0,0,0,9,0,0,3,4],			
+	[0,0,0,3,7,0,2,0,0],		
+	[1,0,0,0,2,5,0,8,0],		
+	[0,2,7,0,0,0,9,6,0],		
+	[0,5,0,6,3,0,0,0,1],		
+	[0,0,6,0,8,9,0,0,0],
+	[2,9,0,0,4,0,0,0,0],
+	[7,0,0,0,0,1,5,9,8]					
+]
+
+var t2 = 
+[
+	[8,0,0,0,0,0,0,0,0],
+	[0,0,3,6,0,0,0,0,0],
+	[0,7,0,0,9,0,2,0,0],
+	[0,5,0,0,0,7,0,0,0],
+	[0,0,0,0,4,5,7,0,0],
+	[0,0,0,1,0,0,0,3,0],
+	[0,0,1,0,0,0,0,6,8],
+	[0,0,8,5,0,0,0,1,0],
+	[0,9,0,0,0,0,4,0,0]
+]
+
+var t3 = 
+[
+	[1,0,0,0],
+	[0,0,3,1],
+	[2,4,1,3],
+	[3,1,4,2]
+]
+
+// console.log(get_available_value_list(world_hardest_sudoku2, 1, 1));
+
 // console.log(correct_sudoku(test))
-var test_soduku = generate_random_sudoku();
-console.log(test_soduku);
-solve_sudoku(test_soduku);
+// var test_soduku = generate_random_sudoku();
+// console.log(test_soduku);
+console.log(solve_sudoku(t2));
 
 
 
